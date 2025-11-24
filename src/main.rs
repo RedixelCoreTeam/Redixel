d39fb1d8e225f96c096e60c988d55413eaff8076
Renan Gomes <renango2015@gmail.com>
2025-11-24 03:43:02 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod engine;
+
+use engine::plataform::window::WindowManager;
+use std::error::Error;
+use winit::event_loop::EventLoop;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let event_loop: EventLoop = EventLoop::new()?;
+    let window: WindowManager = WindowManager::default();
+    event_loop.run_app(window);
+    Ok(())
+}

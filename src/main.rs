dda6a7f8f5ab73a7c4348e789724fe6f5d58e866
Renan Gomes <renango2015@gmail.com>
2025-11-25 03:13:30 -0300

    add basic logging

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::error::Error;
 use winit::event_loop::EventLoop;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
     let event_loop: EventLoop = EventLoop::new()?;
     let window: WindowManager = WindowManager::default();
     event_loop.run_app(window);

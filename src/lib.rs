a8e85f9237e1285eb75f967ed219c8c36738a24f
Renan Gomes <renango2015@gmail.com>
2025-11-26 22:33:20 -0300

    add logger

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use winit::error::EventLoopError;
 use winit::event_loop::{ControlFlow, EventLoop};
 
 pub fn init() -> Result<(), EventLoopError> {
+    env_logger::init();
     let event_loop: EventLoop = EventLoop::new()?;
     event_loop.set_control_flow(ControlFlow::Poll);
     let runtime: Runtime = Runtime::default();

3c9280b06a74d975d8077d552d2f600c9ec86051
Renan Gomes <renango2015@gmail.com>
2025-11-29 20:49:02 -0300

    improve renderer structure

--- src/engine/graphics/renderer.rs
+++ src/engine/graphics/renderer.rs
@@ -1,126 +1,70 @@
-use std::sync::Arc;
+use std::{error::Error, sync::Arc};
 use wgpu::{
-    Device, ExperimentalFeatures, Queue, Surface, SurfaceConfiguration, SurfaceError, Trace,
+    Color, CommandEncoder, CommandEncoderDescriptor, LoadOp, Operations, RenderPassColorAttachment,
+    RenderPassDescriptor, StoreOp, SurfaceError, SurfaceTexture, TextureView, TextureViewDescriptor,
 };
-use winit::window::Window;
+use winit::{dpi::PhysicalSize, window::Window};
+
+use super::renderer_device::RendererDevice;
 
 #[derive(Debug)]
 pub struct Renderer {
-    surface: Surface<'static>,
-    device: Device,
-    queue: Queue,
-    config: SurfaceConfiguration,
+    renderer_device: RendererDevice,
 }
 
 impl Renderer {
-    pub async fn new(window: Arc<dyn Window>) -> Self {
-        let size: winit::dpi::PhysicalSize<u32> = window.outer_size();
+    pub async fn new(window: Arc<dyn Window>) -> Result<Self, Box<dyn Error>> {
+        let renderer_device: RendererDevice = RendererDevice::new(window).await?;
+        Ok(Self { renderer_device })
+    }
 
-        let instance: wgpu::Instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
-            backends: wgpu::Backends::all(),
-            ..Default::default()
-        });
-
-        let surface: Surface<'_> = instance.create_surface(window.clone()).unwrap();
-
-        let adapter: wgpu::Adapter = instance
-            .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::HighPerformance,
-                compatible_surface: Some(&surface),
-                force_fallback_adapter: false,
-            })
-            .await
-            .expect("Failed to find suitable GPU adapter");
-
-        let (device, queue): (Device, Queue) = adapter
-            .request_device(&wgpu::DeviceDescriptor {
-                required_features: wgpu::Features::empty(),
-                required_limits: wgpu::Limits::default(),
-                memory_hints: wgpu::MemoryHints::Performance,
-                trace: Trace::Off,
-                experimental_features: ExperimentalFeatures::disabled(),
-                label: Some("RedPixel Device"),
-            })
-            .await
-            .unwrap();
-
-        let surface_caps: wgpu::SurfaceCapabilities = surface.get_capabilities(&adapter);
-        let surface_format: wgpu::TextureFormat = surface_caps
-            .formats
-            .iter()
-            .copied()
-            .find(|f| f.is_srgb())
-            .unwrap_or(surface_caps.formats[0]);
-
-        let config: wgpu::wgt::SurfaceConfiguration<Vec<wgpu::TextureFormat>> =
-            wgpu::SurfaceConfiguration {
-                usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-                format: surface_format,
-                width: size.width,
-                height: size.height,
-                present_mode: wgpu::PresentMode::Fifo,
-                alpha_mode: surface_caps.alpha_modes[0],
-                view_formats: vec![],
-                desired_maximum_frame_latency: 2,
-            };
-
-        surface.configure(&device, &config);
-
-        Self {
-            surface,
-            device,
-            queue,
-            config,
+    pub fn resize(&mut self, new_size: PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.renderer_device.config.width = new_size.width;
+            self.renderer_device.config.height = new_size.height;
+            self.renderer_device
+                .surface
+                .configure(&self.renderer_device.device, &self.renderer_device.config);
         }
     }
 
-    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        if new_size.width > 0 && new_size.height > 0 {
-            self.config.width = new_size.width;
-            self.config.height = new_size.height;
-            self.surface.configure(&self.device, &self.config);
-        }
+    fn render_pass(&self, encoder: &mut CommandEncoder, view: &TextureView) {
+        encoder.begin_render_pass(&RenderPassDescriptor {
+            label: Some("RENDER_PASS"),
+            depth_stencil_attachment: None,
+            occlusion_query_set: None,
+            timestamp_writes: None,
+            color_attachments: &[Some(RenderPassColorAttachment {
+                view,
+                depth_slice: None,
+                resolve_target: None,
+                ops: Operations {
+                    load: LoadOp::Clear(Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 1.0,
+                    }),
+                    store: StoreOp::Store,
+                },
+            })],
+        });
     }
 
     pub fn render(&mut self) -> Result<(), SurfaceError> {
-        let output: wgpu::SurfaceTexture = self.surface.get_current_texture()?;
-        let view: wgpu::TextureView = output
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
+        let output: SurfaceTexture = self.renderer_device.surface.get_current_texture()?;
+        let view: TextureView = output.texture.create_view(&TextureViewDescriptor::default());
 
-        let mut encoder: wgpu::CommandEncoder =
-            self.device
-                .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                    label: Some("Render Encoder"),
+        let mut encoder: CommandEncoder =
+            self.renderer_device
+                .device
+                .create_command_encoder(&CommandEncoderDescriptor {
+                    label: Some("RENDER_ENCODER"),
                 });
 
-        {
-            let _render_pass: wgpu::RenderPass<'_> =
-                encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                    label: Some("Render Pass"),
-                    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                        depth_slice: None,
-                        view: &view,
-                        resolve_target: None,
-                        ops: wgpu::Operations {
-                            load: wgpu::LoadOp::Clear(wgpu::Color {
-                                r: 0.1,
-                                g: 0.2,
-                                b: 0.3,
-                                a: 1.0,
-                            }),
-                            store: wgpu::StoreOp::Store,
-                        },
-                    })],
-                    depth_stencil_attachment: None,
-                    occlusion_query_set: None,
-                    timestamp_writes: None,
-                });
-        }
-
-        self.queue.submit(std::iter::once(encoder.finish()));
+        self.render_pass(&mut encoder, &view);
+        self.renderer_device.queue.submit(std::iter::once(encoder.finish()));
         output.present();
-
         Ok(())
     }
 }

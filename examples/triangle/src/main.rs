0020c260729447e3dcf2bb0e68a72f6d3126c0bf
Renan Gomes <renango2015@gmail.com>
2026-06-01 01:36:57 -0300

    docs: add pong example

--- examples/triangle/src/main.rs
+++ examples/triangle/src/main.rs
@@ -37,7 +37,7 @@ fn main() {
     #[cfg(target_arch = "wasm32")]
     {
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-        console_log::init_with_level(log::Level::Info).expect("Falha ao iniciar logger WASM");
+        console_log::init_with_level(log::Level::Info).expect("Failed to initialize WASM logger");
     }
 
     #[cfg(not(target_arch = "wasm32"))]
